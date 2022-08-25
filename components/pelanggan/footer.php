0cfeae43a6a85de254ab087f4c2cfaf2456c3cad
rianwp <rianwp2015@gmail.com>
2022-08-25 17:30:03 +0700

    Create footer.php

---
+++ components/pelanggan/footer.php
@@ -0,0 +1,15 @@
+<footer class="bg-dark text-center text-white">
+  <div class="container p-4 pb-0">
+    <section class="mb-4">
+      <a class="mx-2" href="#" role="button">
+        <i class="bi bi-instagram"></i>
+      </a>
+      <a class="mx-2" href="https://github.com/rianwp" role="button">
+        <i class="bi bi-github"></i>
+      </a>
+    </section>
+  </div>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    Dibuat oleh Adrian Wahyu Prasetya
+  </div>
+</footer>
