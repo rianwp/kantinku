8210674eea9887ac1a0a1bcf9ce84f5994ce82df
rianwp <rianwp2015@gmail.com>
2022-08-25 17:30:18 +0700

    Create editnama_pelanggan.php

---
+++ process/pelanggan/editnama_pelanggan.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require '../connect.php';
+$id = $_SESSION['id_pelanggan'];
+$nama = $_POST['nama'];
+$konfirmasi = $_POST['konfirmasi'];
+
+$sqlvalidateedit = "SELECT password_pelanggan FROM pelanggan WHERE id_pelanggan='$id'";
+$validateedit = mysqli_query($conn, $sqlvalidateedit);
+$resultvalidateedit = mysqli_fetch_assoc($validateedit);
+
+$sqleditnama = "UPDATE pelanggan SET nama_pelanggan='$nama' WHERE id_pelanggan='$id'";
+
+if(password_verify($konfirmasi, $resultvalidateedit['password_pelanggan'])){
+    $success = true;
+    $msg = "Nama anda sudah terganti";
+    $type = "success";
+    mysqli_query($conn, $sqleditnama);
+    echo json_encode(array("success" => $success, "msg" => $msg, "type" => $type));
+} else{
+    $success = false;
+    $msg = "Password anda salah";
+    $type = "error";
+    echo json_encode(array("success" => $success, "msg" => $msg, "type" => $type));
+}
