d30b2093ece082a66f97dc0e94a7bf50af00beb9
rianwp <rianwp2015@gmail.com>
2022-08-25 17:30:37 +0700

    Create login.js

---
+++ script/pelanggan/login.js
@@ -0,0 +1,50 @@
+let form = document.getElementById('form-login')
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let msgErrorEmail = document.getElementById('email-error')
+    let msgErrorPassword = document.getElementById('password-error')
+
+    let checkEmail = document.getElementById('email').value
+    let checkPassword = document.getElementById('password').value
+  
+    if (checkEmail != "" && checkPassword != ""){
+        msgErrorEmail.innerText = ""
+        msgErrorPassword.innerText = ""
+
+        const loginData = new URLSearchParams
+        loginData.append("email", checkEmail)
+        loginData.append("password", checkPassword)
+
+        const authLogin = async function (){
+            const response = await fetch("../process/pelanggan/loginauth_pelanggan.php", {
+                method : "POST",
+                body : loginData
+            })
+            const result = await response.json()
+
+            return result 
+        }
+        authLogin().then(response => {
+            if(response.loggedIn){
+                swal("Login Berhasil", response.msg, response.type)
+                setTimeout(() => {
+                    window.location.href = "/"
+                },1500)
+            } else{
+                swal("Login Gagal", response.msg, response.type)
+            }
+        })
+    } else{
+        if(checkEmail == ""){
+            msgErrorEmail.innerText = "Email tidak boleh kosong"
+        } else{
+            msgErrorEmail.innerText = ""
+        }
+        if(checkPassword == ""){
+            msgErrorPassword.innerText = "Password tidak boleh kosong"
+        } else{
+            msgErrorPassword.innerText = ""
+        }
+    }
+})
