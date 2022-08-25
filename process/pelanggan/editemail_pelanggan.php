f19152a341feb5b902ad2d3a1277b4a63d2cacc0
rianwp <rianwp2015@gmail.com>
2022-08-25 17:30:14 +0700

    Create editemail_pelanggan.php

---
+++ process/pelanggan/editemail_pelanggan.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require '../connect.php';
+$id = $_SESSION['id_pelanggan'];
+$email = $_POST['email'];
+$konfirmasi = $_POST['konfirmasi'];
+
+$sqlvalidateedit = "SELECT email_pelanggan FROM pelanggan WHERE id_pelanggan='$id'";
+$validateedit = mysqli_query($conn, $sqlvalidateedit);
+$resultvalidateedit = mysqli_fetch_assoc($validateedit);
+
+$sqleditemail = "UPDATE pelanggan SET email_pelanggan='$email' WHERE id_pelanggan='$id'";
+
+if(password_verify($konfirmasi, $resultvalidateedit['password_pelanggan'])){
+    $success = true;
+    $msg = "Email anda sudah terganti";
+    $type = "success";
+    mysqli_query($conn, $sqleditemail);
+    echo json_encode(array("success" => $success, "msg" => $msg, "type" => $type));
+} else{
+    $success = false;
+    $msg = "Password anda salah";
+    $type = "error";
+    echo json_encode(array("success" => $success, "msg" => $msg, "type" => $type));
+}
