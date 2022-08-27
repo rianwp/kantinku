9c7e6d86f74cc8f5761975574dc154067fbd8a37
rianwp <rianwp2015@gmail.com>
2022-08-27 16:42:41 +0700

    Update navbar.php

--- components/pelanggan/navbar.php
+++ components/pelanggan/navbar.php
@@ -17,7 +17,7 @@
       </div>
       <div class="navbar-nav">
         <?php if(isset($_SESSION['isLoggedIn_pelanggan'])){
-          require 'process/connect.php';
+          require 'process/config.php';
           $id_pelanggan = $_SESSION['id_pelanggan'];
           $sqlprofile = "SELECT nama_pelanggan, foto_pelanggan FROM pelanggan WHERE id_pelanggan='$id_pelanggan'";
           $resultprofile= mysqli_query($conn, $sqlprofile);
@@ -30,7 +30,13 @@
           <button class="dropdown d-inline-block border-0 bg-transparent p-0">
             <a role="button" id="profileDropdown" data-bs-toggle="dropdown" aria-expanded="false" class="btn btn-outline-success text-success rounded-pill px-3 mx-lg-1 my-lg-0 my-1 w-100"><i class="bi bi-person-fill"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
-              <li class="text-center px-1"><img src="data:image/jpeg;base64,<?= base64_encode($foto) ?>" class="rounded-circle foto-profile"></li>
+              <li class="text-center px-1">
+                <?php if($foto == ""){ ?>
+                  <img src="../assets/user.png" class="rounded-circle foto-profile">
+                <?php } else{ ?>
+                  <img src="data:image/jpeg;base64,<?= base64_encode($foto) ?>" class="rounded-circle foto-profile">
+                <?php } ?>
+              </li>
               <li class="text-center pt-2 ket-nama text-muted">Pelanggan</li>
               <li class="text-center pb-2 px-1 border-bottom border-1 font-nama"><?= $nama ?></li>
               <li class="mt-2"><a class="dropdown-item" href="/profile">Profile</a></li>
