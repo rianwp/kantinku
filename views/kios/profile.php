d55cf42a420c328700dcb566420978ceb80758d2
rianwp <rianwp2015@gmail.com>
2022-08-27 16:43:59 +0700

    Create profile.php

---
+++ views/kios/profile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <style><?php require 'style/kios/profile.css' ?></style>
+    <style><?php require 'style/font.css' ?></style>
+    <title>Profile</title>
+</head>
+<body>
+    <?php require 'components/kios/navbar.php' ?>
+    <?php require 'components/kios/sidebar.php' ?>
+    <main>
+        <div class="container-fluid mx-auto p-3">
+          <h1 class="fw-bold">Profile</h1>
+        </div>
+    </main>
+</body>
+</html>
